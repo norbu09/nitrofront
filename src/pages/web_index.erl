19c6f86032a48ed3b0c09b3769af7abe7e9ef59c
Lenz Gschwendtner <lenz@springtimesoft.com>
2009-03-02 16:36:01 +1300

    steal nice layouts from nitrogen

--- src/pages/web_index.erl
+++ src/pages/web_index.erl
@@ -2,20 +2,20 @@
 -include_lib ("nitrogen/include/wf.inc").
 -compile(export_all).
 
-main() -> 
-	#template { file="./wwwroot/template.html"}.
+main() -> #template { file="./wwwroot/onecolumn.html", bindings=[
+  {'Group', index},
+  {'Item', linklist}
+]}.
 
-title() ->
-	"list of links".
+title() -> "list of links".
+headline() -> "List of links".
 
 body() ->
     [Key,Value] = links:get_all(),
     io:format("DEBUG: ~s -> ~s~n",[Key, Value]),
-	#label{text="list of links."},
+    #h3 { text="the links so far" },
     #p{},
     #label { text=Key },
     #value { text=Value }.
 
-
-	
 event(_) -> ok.
